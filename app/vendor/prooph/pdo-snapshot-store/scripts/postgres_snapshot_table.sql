d059f5648179464ccdc5b870551a4b652808d342
zawiszaty <szymonciom@gmail.com>
2019-03-17 14:20:27 +0100

    fix

--- app/vendor/prooph/pdo-snapshot-store/scripts/postgres_snapshot_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE snapshots (
-  aggregate_id VARCHAR(150) NOT NULL,
-  aggregate_type VARCHAR(150) NOT NULL,
-  last_version INT NOT NULL,
-  created_at CHAR(26) NOT NULL,
-  aggregate_root BYTEA,
-  PRIMARY KEY (aggregate_id)
-);
