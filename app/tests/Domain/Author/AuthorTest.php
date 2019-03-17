3c2ef396ad23c5463e6a4b5105928104658411ed
zawiszaty <szymonciom@gmail.com>
2019-03-17 20:25:55 +0100

    author delete tdd

--- app/tests/Domain/Author/AuthorTest.php
+++ app/tests/Domain/Author/AuthorTest.php
@@ -5,6 +5,8 @@ namespace Tests\Domain\Author;
 use App\Domain\Author\Author;
 use App\Domain\Author\Events\AuthorNameWasChanged;
 use App\Domain\Author\Events\AuthorWasCreated;
+use App\Domain\Author\Events\AuthorWasDeleted;
+use App\Domain\Category\Exception\SameNameException;
 use App\Domain\Common\ValueObject\AggregateRootId;
 use App\Domain\Common\ValueObject\Name;
 use Tests\TestCase;
@@ -42,6 +44,47 @@ class AuthorTest extends TestCase
         $expectedPayload = [
             'name' => 'test2',
         ];
+        $this->assertEquals($expectedPayload, $events[0]->payload());
+    }
+
+    function test_it_change_same_name()
+    {
+        $this->expectException(SameNameException::class);
+        $author = Author::create(AggregateRootId::generate(), Name::fromString('test'));
+        $this->assertInstanceOf(Author::class, $author);
+        $events = $this->popRecordedEvent($author);
+        $this->assertEquals(1, \count($events));
+        $this->assertInstanceOf(AuthorWasCreated::class, $events[0]);
+        $expectedPayload = [
+            'name' => 'test',
+        ];
+        $this->assertEquals($expectedPayload, $events[0]->payload());
+        $author->changeName('test');
+        $events = $this->popRecordedEvent($author);
+        $this->assertEquals(1, \count($events));
+        $this->assertInstanceOf(AuthorNameWasChanged::class, $events[0]);
+        $expectedPayload = [
+            'name' => 'test2',
+        ];
+        $this->assertEquals($expectedPayload, $events[0]->payload());
+    }
+
+    function test_it_delete_author()
+    {
+        $author = Author::create(AggregateRootId::generate(), Name::fromString('test'));
+        $this->assertInstanceOf(Author::class, $author);
+        $events = $this->popRecordedEvent($author);
+        $this->assertEquals(1, \count($events));
+        $this->assertInstanceOf(AuthorWasCreated::class, $events[0]);
+        $expectedPayload = [
+            'name' => 'test',
+        ];
+        $this->assertEquals($expectedPayload, $events[0]->payload());
+        $author->delete();
+        $events = $this->popRecordedEvent($author);
+        $this->assertEquals(1, \count($events));
+        $this->assertInstanceOf(AuthorWasDeleted::class, $events[0]);
+        $expectedPayload = [];
         $this->assertEquals($expectedPayload, $events[0]->payload());
     }
 }
