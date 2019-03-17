d9972b5084aa34c7a886faa856880e85fcdc67fd
zawiszaty <szymonciom@gmail.com>
2019-03-17 20:47:41 +0100

    book change name tdd

--- app/tests/Domain/Book/BookTest.php
+++ app/tests/Domain/Book/BookTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Domain\Book;
 
 use App\Domain\Book\Book;
+use App\Domain\Book\Event\BookNameWasChanged;
 use App\Domain\Book\Event\BookWasCreated;
 use App\Domain\Book\ValueObject\Description;
 use App\Domain\Common\ValueObject\AggregateRootId;
@@ -25,6 +26,32 @@ class BookTest extends TestCase
         $expectedPayload = [
             'name' => 'test',
             'description' => 'test',
+        ];
+        $this->assertEquals($expectedPayload, $events[0]->payload());
+    }
+
+    function test_book_it_change_name()
+    {
+        $book = Book::create(
+            AggregateRootId::generate(),
+            Name::fromString('test'),
+            Description::fromString('test')
+        );
+        $this->assertInstanceOf(Book::class, $book);
+        $events = $this->popRecordedEvent($book);
+        $this->assertEquals(1, \count($events));
+        $this->assertInstanceOf(BookWasCreated::class, $events[0]);
+        $expectedPayload = [
+            'name' => 'test',
+            'description' => 'test',
+        ];
+        $this->assertEquals($expectedPayload, $events[0]->payload());
+        $book->changeName('test');
+        $events = $this->popRecordedEvent($book);
+        $this->assertEquals(1, \count($events));
+        $this->assertInstanceOf(BookNameWasChanged::class, $events[0]);
+        $expectedPayload = [
+            'name' => 'test',
         ];
         $this->assertEquals($expectedPayload, $events[0]->payload());
     }
